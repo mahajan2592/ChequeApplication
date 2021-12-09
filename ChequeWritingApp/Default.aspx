87a16e02843adf89544e71f69047af79f75d9662
mahajan2592 <95884003+mahajan2592@users.noreply.github.com>
2021-12-10 02:43:28 +0530

    Add files via upload

---
+++ ChequeWritingApp/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChequeWritingApp._Default" %>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h4>Cheque Calculator</h4>
+        <br />
+         <label>Name</label> 
+         <asp:TextBox ID="ChequeName" runat="server"></asp:TextBox>
+         <label>Value</label>
+        <asp:TextBox ID="ChequeValue" runat="server" ></asp:TextBox>
+        <asp:Button ID="submitInput" runat="server" Text="Submit" OnClick="submitInput_Click" />
+
+        <br /><br />
+        <p><asp:Label runat="server" ID="PersonName"></asp:Label></p>
+       <p><asp:label ID="NumberWithDecimal" runat="server"></asp:label><asp:label ID="NumberInWords" runat="server"></asp:label></p> 
+    </div>
+   
+    
+
+</asp:Content>
---
+++ ChequeWritingApp/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChequeWritingApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitInput_Click(object sender, EventArgs e)
+        {
+            localhost.ChequeWritingWebService chqService = new localhost.ChequeWritingWebService();
+            string Name = ChequeName.Text.ToString();
+            string Number = ChequeValue.Text.ToString();
+            if (Number.Length < 12)
+            {
+                if (Number.Contains('.'))
+                {
+                    string DecimalPlaceNumber = Number.Substring(Number.LastIndexOf('.') + 1);
+                    string NonDecimalPlaceNumber= Number.Substring(0, Number.IndexOf('.'));
+                    if (NonDecimalPlaceNumber.Length < 6)
+                    {
+                        string ConvertedNumberToWord = chqService.ChequeService(NonDecimalPlaceNumber, Name);
+                        if (!(string.IsNullOrEmpty(Name)))
+                        {
+                            PersonName.Text = Name;
+                        }
+                        NumberWithDecimal.Text = ConvertedNumberToWord.ToUpper().ToString() +" DOLLARS AND ";
+                    }
+                    if (DecimalPlaceNumber.Length < 6)
+                    {
+                        string ConvertedNumberToWord = chqService.ChequeService(DecimalPlaceNumber, Name);
+                        
+                            NumberInWords.Text = ConvertedNumberToWord.ToUpper().ToString().Replace("AND CENTS", "") + " CENTS";
+                       
+                    }
+                }
+                else if(Number.Length < 6)
+                {
+                    string ConvertedNumberToWord = chqService.ChequeService(Number, Name);
+                    if (!(string.IsNullOrEmpty(Name)))
+                    {
+                        PersonName.Text = Name;
+                    }
+                    NumberInWords.Text = ConvertedNumberToWord.ToString().Replace("AND DOLLARS","") + " DOLLARS";
+                }
+            }
+            else
+            {
+                NumberWithDecimal.Text = "";
+                NumberInWords.Text = "Please Enter the Number withing the range of 5 digits before and after decimal places ";
+            }
+            
+        }
+    }
+}
